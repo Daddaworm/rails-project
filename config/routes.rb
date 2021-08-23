98bfebf0d8b9cb752df84f0f8b23c7616fb2217e
Ethan Rodriguez <ethan.rodriguez@mac.com>
2021-08-23 19:20:40 -0400

    testing

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,7 @@ Rails.application.routes.draw do
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
+  
 end
